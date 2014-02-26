02c9d9235e31a37fa5b0e449b7ea61b347dcc5cf
John Frimmel <johnfrimmel@gmail.com>
2014-02-26 13:59:01 -0500

    removes deprecated templates

--- modules/data_snapshots/templates/data-snapshot-sidebar.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Move templates into display suite
-?>
-<div id="datasource-snapshot-block">
-    <div><b>Data Source Name:</b><?php print $data_source_name; ?></div>
-    <div><b>Data Source Number:</b><?php print $data_source_number; ?></div>
-</div>
