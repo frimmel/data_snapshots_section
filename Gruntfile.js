1407a028f53971c5cb41373d49770ad3387ad104
John Frimmel <johnfrimmel@gmail.com>
2014-03-05 17:23:55 -0500

    updates grunt info

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,57 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
 	    pkg: grunt.file.readJSON('package.json'),
+	    banner: '/*! <%= pkg.name %>\n' +
+	            ' * Copyright 2014 University of North Carolina at Asheville\n' +
+           	    ' * Released under the MIT license\n' +
+	            ' */',
+	    concat: {
+		dataSnapshotJs: {
+		    src: [
+			  "modules/data_snapshots/js/data-snapshots.js"
+		    ],
+		    dest: 'modules/data_snapshots/build/data-snapshots.js'
+		},
+		dataSnapshotCss: {
+		    src: [
+			  "modules/data_snapshots/css/data-snapshots.css"
+                    ],
+		    dest: 'modules/data_snapshots/build/data-snapshots.css'
+		}
+	    },
 	    uglify: {
 		options: {
-		    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		    banner: '<%= banner %>' + '\n'
 		},
 		build: {
-		    src: 'modules/data_snapshots/js/data_snapshots.js',
-		    dest: 'modules/data_snapshots/js/data_snapshots.min.js'
+		    src: 'modules/data_snapshots/build/data-snapshots.js',
+		    dest: 'modules/data_snapshots/build/data-snapshots.min.js'
 		}
+	    },
+	    cssmin: {
+		dataSnapshotCss: {
+		    options: {
+			keepSpecialComments: 0,
+			banner: '<%= banner %>'
+		    },
+                    src: "modules/data_snapshots/build/data-snapshots.css",
+                    dest: "modules/data_snapshots/build/data-snapshots.min.css"
+                },
+		dataSourceCss: {
+		    options: {
+			keepSpecialComments: 0,
+			banner: '<%= banner %>'
+		    },
+                    src: "modules/data_snapshots_data_source/css/data-source.css",
+                    dest: "modules/data_snapshots_data_source/build/data-source.min.css"
+                }
 	    }
 	});
 
-    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
