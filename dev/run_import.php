79f8c5c70e791c36762633ffccf86bde99a4ee57
Mark Phillips <embeepea@git>
2014-04-28 22:06:48 -0400

    add several new data sets, change to import from temporary AWS server

---
+++ dev/run_import.php
@@ -0,0 +1,7 @@
+<?php
+
+# Running this script with "drush scr run_import.php" does the same thing
+# as clicking the "Run Import Now" button in the Data Snapshots Module admin settings screen;
+# i.e. it causes the Data Snapshots module to do an import run.
+
+data_snapshots_run_import();
